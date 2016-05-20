7008e69bf1553b22b359135bb787901903ce5eef
Bill Yung <bill.ken.yung@gmail.com>
2016-05-20 17:51:54 -0400

    Separated sections

--- public/scripts/animation.js
+++ public/scripts/animation.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
     $('#carousel-photoroll').carousel({
-        interval: 3000,
+        interval: 5000,
         pause: false
     });
 
     $('.anchor-scroll').anchorScroll({
         scrollSpeed: 800,
         offsetTop: 50
+    });
+
+    $('.nav a').on('click', function(){
+        $('.navbar-toggle').click()
     });
 });
