6836169ed9e1b4305237c09d8f9118547a207103
Bill Yung <bill.ken.yung@gmail.com>
2016-05-23 16:42:06 -0400

    Added a recipes page and database to store and render all recipes

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var app = express();
+var mongodb = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://heroku_7mgmv4x2:due2qgtm2gj5k1fatnl5246enk@ds023912.mlab.com:23912/heroku_7mgmv4x2';
+var MongoClient = mongodb.MongoClient, format = require('util').format;
+var db = MongoClient.connect(mongoUri, function (error, databaseConnection) {
+    db = databaseConnection;
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,8 +16,54 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-app.get('/', function(request, response) {
+/* Renders the home page */
+app.get('/', function (request, response) {
     response.render('index');
+});
+
+/* Renders the recipe page depending on the meal type */
+app.get('/recipes', function (request, response) {
+    var meal_type = request.query.meal_type;
+
+    db.collection('recipes').find({"meal_type":meal_type}).toArray(function (error, recipes) {
+        if ( !error ) {
+            response.render('recipes', {data: recipes});
+        }
+        else {
+            response.sendStatus(500);
+        }
+    });
+});
+
+app.post('/addRecipe', function (request, response) {
+    var meal_type = request.body.meal_type;
+    var dish = request.body.dish;
+    var description = request.body.description;
+    var recipe = request.body.recipe;
+
+    // Creating object to insert
+    var toInsert = {
+        "meal_type" : meal_type,
+        "dish" : dish,
+        "description" : description,
+        "recipe" : recipe
+    };
+
+    db.collection('recipes').insert(toInsert, function (error, saved) {
+        if ( !error ) {
+            db.collection('recipes').find().toArray(function (error, recipes) {
+                if ( !error ) {
+                    response.send(recipes);
+                }
+                else {
+                    response.sendStatus(500);
+                }
+            });
+        }
+        else {
+            response.sendStatus(500);
+        }
+    });
 });
 
 app.listen(app.get('port'), function() {
